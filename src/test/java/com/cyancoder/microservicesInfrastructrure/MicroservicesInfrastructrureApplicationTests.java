d91fc1a06797df6f9cbc3e8dbf4a41cee663ae77
Administrator <admin@example.com>
2023-09-17 14:12:32 +0330

    first commit

---
+++ src/test/java/com/cyancoder/microservicesInfrastructrure/MicroservicesInfrastructrureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyancoder.microservicesInfrastructrure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesInfrastructrureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
